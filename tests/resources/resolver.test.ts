a9d6a780009cb158b855741d2b2a80177345f3ed
James Bower <bower.james1996@gmail.com>
2026-04-14 23:33:41 +0100

    chore: remove unused imports from test files
    Remove unused imports: stat from loader.test.ts, mkdir/writeFile from
    resolver.test.ts.

--- tests/resources/resolver.test.ts
+++ tests/resources/resolver.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test, beforeEach, afterEach } from "bun:test";
-import { mkdtemp, rm, mkdir, writeFile } from "node:fs/promises";
+import { mkdtemp, rm } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
 import {
